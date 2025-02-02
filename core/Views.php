21301d1d158acd00e611d5efdf00a94c6ab2f3eb
Diego Berrios <diego.berrios@hyros.com>
2025-02-01 21:25:52 -0400

    [RAC-MVC] Implementing views renderer

---
+++ core/Views.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\core;
+
+class Views {
+	public static function renderView($name) {
+		$filepath = dirname(__DIR__) . '/views/' . $name;
+
+		if(!file_exists($filepath)) {
+			echo 'View not found';
+			return;
+		}
+		
+		$view = require_once($filepath);
+		echo $view;
+
+	}
+}
+
--- public/index.php
+++ public/index.php
@@ -2,11 +2,12 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use app\core\Application;
+use app\core\Views;
 
 $app = new Application();
 
 $app -> get('/raccoon', function(){ 
-	echo "Hello raccoon!";
+	Views::RenderView('test.php');
 });
 
 $app -> run();
